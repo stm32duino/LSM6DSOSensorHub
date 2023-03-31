21d5fc77a46cbdb26ed444ef10b4dfc30d697285
Lorenzo Bini <lorenzo.bini.97@proton.me>
2023-03-31 11:38:14 +0200

    Add ability to retrieve device id from addDevice() function so it must not be manually tracked,. Astyle and auto-format pass.

--- examples/SensorHubExample/SensorHubExample.ino
+++ examples/SensorHubExample/SensorHubExample.ino
@@ -1,77 +1,77 @@
 /**
  ******************************************************************************
- * @file    SensorHubExample.ino
- * @author  Lorenzo Bini
- * @version V1.0.0
- * @date    February 2023
- * @brief   Library for interacting with the LSM6DSO Sensor Hub function
+   @file    SensorHubExample.ino
+   @author  Lorenzo Bini
+   @version V1.1.0
+   @date    March 2023
+   @brief   Library for interacting with the LSM6DSO Sensor Hub function
  ******************************************************************************
- * @attention
- *
- * <h2><center>&copy; COPYRIGHT(c) 2019 STMicroelectronics</center></h2>
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- *   1. Redistributions of source code must retain the above copyright notice,
- *      this list of conditions and the following disclaimer.
- *   2. Redistributions in binary form must reproduce the above copyright notice,
- *      this list of conditions and the following disclaimer in the documentation
- *      and/or other materials provided with the distribution.
- *   3. Neither the name of STMicroelectronics nor the names of its contributors
- *      may be used to endorse or promote products derived from this software
- *      without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
+   @attention
+
+   <h2><center>&copy; COPYRIGHT(c) 2023 STMicroelectronics</center></h2>
+
+   Redistribution and use in source and binary forms, with or without modification,
+   are permitted provided that the following conditions are met:
+     1. Redistributions of source code must retain the above copyright notice,
+        this list of conditions and the following disclaimer.
+     2. Redistributions in binary form must reproduce the above copyright notice,
+        this list of conditions and the following disclaimer in the documentation
+        and/or other materials provided with the distribution.
+     3. Neither the name of STMicroelectronics nor the names of its contributors
+        may be used to endorse or promote products derived from this software
+        without specific prior written permission.
+
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+   AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+   IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+   DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+   FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+   DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+   SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+   CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+   OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
  ******************************************************************************
- */
+*/
 
 
 /*
 
-LSM6DSOHub Arduino Library
-By Lorenzo Bini at STMicroelectronics
+  LSM6DSOHub Arduino Library
+  By Lorenzo Bini at STMicroelectronics
 
-This library provides functionality for interacting with the Sensor Hub functionality on LSM6DSO sensors.
+  This library provides functionality for interacting with the Sensor Hub functionality on LSM6DSO sensors.
 
-BOARD SETUP
-If you are using a X-NUCLEO-IKS01A3 board, you must correctly wire up the sensor hub bus to the other sensors. To do this, short the middle two pins for each of the two jumper sets JP8 and JP7, which look like this:
+  BOARD SETUP
+  If you are using a X-NUCLEO-IKS01A3 board, you must correctly wire up the sensor hub bus to the other sensors. To do this, short the middle two pins for each of the two jumper sets JP8 and JP7, which look like this:
 
-JP8   * * * *
-JP7   * * * *
+  JP8   * * * *
+  JP7   * * * *
 
-Short as following:
+  Short as following:
 
-JP8   * O-O *
-JP7   * O-O *
+  JP8   * O-O *
+  JP7   * O-O *
 
-* represents a pin
-O-O represents a jumper and the two pins it is shorting
+  represents a pin
+  O-O represents a jumper and the two pins it is shorting
 
-Note that with this setup, normal sensor operation will be unavailable unless specific library functionality is used
+  Note that with this setup, normal sensor operation will be unavailable unless specific library functionality is used
 
-LIBRARY USAGE
-1. Create LSM6DSOHub object by passing it a valid LSM6DSOSensor representing the sensor you wish to use as master
-2. Make sure the master has been initialized by calling its begin function
-3. Initialize the Sensor Hub by calling the resetMaster function
-4. Call the addDevice function for each device you would like to read using the sensor hub, up to four (4) devices. Provide, in this order, the I2C address of the device, the register address you wish to read, and how many times you wish to read the device at each read operation. Note that not all devices automatically increment the register address when multiple reads are issued in one operation.
-5. Call the begin function to star the sensor hub
-6. In your loop function, call waitAndRead whenever you wish to extract a new set of data from your sensors. Note that this function will block until all sensors have been read. Alternatively, you may manually wait and extract data by using the ready and read functions.
-7. To actually obtain the data related to one of your sensors, call getSensor. You must provide, in this order, the sensor number (for example, the first sensor that was added is sensor 0, the second sensor 1, and so on), and a pointer to a uint8_t array which is as long as the number of reads you set for that sensor.
+  LIBRARY USAGE
+  1. Create LSM6DSOHub object by passing it a valid LSM6DSOSensor representing the sensor you wish to use as master
+  2. Make sure the master has been initialized by calling its begin function
+  3. Initialize the Sensor Hub by calling the resetMaster function
+  4. Call the addDevice function for each device you would like to read using the sensor hub, up to four (4) devices. Provide, in this order, the I2C address of the device, the register address you wish to read, and how many times you wish to read the device at each read operation. The function returns the sensor number that is assigned to the device. Note that not all devices automatically increment the register address when multiple reads are issued in one operation.
+  5. Call the begin function to star the sensor hub
+  6. In your loop function, call waitAndRead whenever you wish to extract a new set of data from your sensors. Note that this function will block until all sensors have been read. Alternatively, you may manually wait and extract data by using the ready and read functions.
+  7. To actually obtain the data related to one of your sensors, call getSensor. You must provide, in this order, the sensor number (which is returned by addDevice()), and a pointer to a uint8_t array which is as long as the number of reads you set for that sensor.
 
-For example: a sensor added second with addDevice (0xBE, 0xEF, 4) should be read, after calling waitAndRead, by calling getSensor (1, arr) where arr is a uint8_t arr[4] of length 4.
+  For example: a sensor added second with addDevice (0xBE, 0xEF, 4) should be read, after calling waitAndRead, by calling getSensor (1, arr) where arr is a uint8_t arr[4] of length 4.
 
-USING PASSTHROUGH FUNCTIONALITY
-Before activating the sensor hub with begin(), the enablePassthrough() function may be used to gain normal I2C access to the sensors connected to the hub. Note that in this case, sensor hub operation is impossible. Before enabling the sensor hub, the passthrough feature MUST be disabled by calling disablePassthrough(). Passthrough functions MUST NOT be used once the sensor hub is operational. To regain use of these functions afterwards, the sensor must be fully reset using the resetMaster() function, after which it must be configured again.
+  USING PASSTHROUGH FUNCTIONALITY
+  Before activating the sensor hub with begin(), the enablePassthrough() function may be used to gain normal I2C access to the sensors connected to the hub. Note that in this case, sensor hub operation is impossible. Before enabling the sensor hub, the passthrough feature MUST be disabled by calling disablePassthrough(). Passthrough functions MUST NOT be used once the sensor hub is operational. To regain use of these functions afterwards, the sensor must be fully reset using the resetMaster() function, after which it must be configured again.
 
 */
 
@@ -98,8 +98,7 @@ Before activating the sensor hub with begin(), the enablePassthrough() function 
 #define INT_1 4
 
 
-typedef struct lin_t
-{
+typedef struct lin_t {
   float x0;
   float y0;
   float x1;
@@ -116,14 +115,14 @@ LPS22HHSensor Pressure(&DEV_I2C);
 STTS751Sensor Temp(&DEV_I2C);
 
 
-uint8_t readObj (LSM6DSOSensor &m, uint8_t reg)
+uint8_t readObj(LSM6DSOSensor &m, uint8_t reg)
 {
   uint8_t res = 0xff;
   m.Read_Reg(reg, &res);
-  return res; 
+  return res;
 }
 
-void writeObj (LSM6DSOSensor &m, uint8_t reg, uint8_t data)
+void writeObj(LSM6DSOSensor &m, uint8_t reg, uint8_t data)
 {
   m.Write_Reg(reg, data);
   return;
@@ -137,6 +136,12 @@ static float Linear_Interpolation(lin_t *Lin, float Coeff)
 
 lin_t humLine;
 
+int mag = -1;
+int hum = -1;
+int press = -1;
+int temp1 = -1;
+int temp2 = -1;
+
 void setup()
 {
   // Initialize serial for output.
@@ -147,7 +152,7 @@ void setup()
   DEV_I2C.begin();
 
   Master.begin();
-  
+
   Hub.resetMaster();
 
   // Enable passthrough
@@ -176,8 +181,8 @@ void setup()
   Humidity.ReadReg(0x3a, &lsb1r[0]);
   Humidity.ReadReg(0x3b, &lsb1r[1]);
 
-  lsb0 = (int16_t) ((lsb0r[1] << 8) | lsb0r[0]);
-  lsb1 = (int16_t) ((lsb1r[1] << 8) | lsb1r[0]);
+  lsb0 = (int16_t)((lsb0r[1] << 8) | lsb0r[0]);
+  lsb1 = (int16_t)((lsb1r[1] << 8) | lsb1r[0]);
 
   humLine.x0 = lsb0 * 1.0f;
   humLine.x1 = lsb1 * 1.0f;
@@ -189,23 +194,23 @@ void setup()
   Master.Enable_G();
 
   Hub.disablePassthrough();
-  
+
   delay(100);
 
   // Prepare the sensor hub
-  Hub.addDevice (0x3D, 0x68, 6);                  // Magnetometer sensor LIS2MDL
-  Hub.addDevice (0xBB, 0x28, 5);                  // Pressure sensor LPS22H
+  mag = Hub.addDevice(0x3D, 0x68, 6);                     // Magnetometer sensor LIS2MDL
+  press = Hub.addDevice(0xBB, 0x28, 5);                   // Pressure sensor LPS22H
 #ifndef USE_STTS751
-  Hub.addDevice (0xBF, (0x28 | 0x80U), 2);        // Humidity sensor HTS221
+  hum = Hub.addDevice(0xBF, (0x28 | 0x80U), 2);           // Humidity sensor HTS221
 #else
-  Hub.addDevice (0x95, 0x00, 1);                  // Temperature sensor STTS751 (Note: this has no auto-increment so it will read the same register 3 times)
-  Hub.addDevice (0x95, 0x02, 1);                  // Temperature sensor STTS751 (Note: this has no auto-increment so it will read the same register 3 times)
+  temp1 = Hub.addDevice(0x95, 0x00, 1);                   // Temperature sensor STTS751 (Note: this has no auto-increment so it will read the same register 3 times)
+  temp2 = Hub.addDevice(0x95, 0x02, 1);                   // Temperature sensor STTS751 (Note: this has no auto-increment so it will read the same register 3 times)
 #endif
   // Engage master hub mode
   Hub.begin();
 
   Serial.println("Ready.");
-  
+
   delay(1000);
 }
 
@@ -232,7 +237,7 @@ void loop()
   // Magnetometer
   uint32_t dataMag [6];
 
-  Hub.getSensor (0, dataMag);
+  Hub.getSensor(mag, dataMag);
 
   // We have the data, now do concats to produce the full values
   int16_t x = (dataMag[1] << 8) | dataMag[0];
@@ -243,9 +248,9 @@ void loop()
   int magZ = (int) z;
 
   // Apply sensitivity
-  magX = (int) (magX * 1.5);
-  magY = (int) (magY * 1.5);
-  magZ = (int) (magZ * 1.5);
+  magX = (int)(magX * 1.5);
+  magY = (int)(magY * 1.5);
+  magZ = (int)(magZ * 1.5);
 
   Serial.println("LIS2MDL:");
   Serial.print("Mag = ");
@@ -254,7 +259,7 @@ void loop()
 
   // Pressure and temperature sensor
   uint32_t dataPress [5];
-  Hub.getSensor(1, dataPress);
+  Hub.getSensor(press, dataPress);
 
   int32_t press = (dataPress[2] << 16) | (dataPress[1] << 8) | dataPress[0];
   float fpress = (((float)press) / 4096.0f);
@@ -266,17 +271,17 @@ void loop()
   int16_t pressTemp = (dataPress[4] << 8) | dataPress[3];
   float t = ((float) pressTemp) / 100;
 
-  Serial.println("Temperature = " + String(t));    
+  Serial.println("Temperature = " + String(t));
   Serial.println("");
 
 #ifdef USE_STTS751
   // Temperature Sensor
   uint32_t dataTempHi [1];
-  Hub.getSensor (2, dataTempHi);
+  Hub.getSensor(temp1, dataTempHi);
   uint32_t dataTempLo [1];
-  Hub.getSensor (3, dataTempLo);
+  Hub.getSensor(temp2, dataTempLo);
 
-  uint32_t tempRaw = (dataTempHi[0] << 8 ) | dataTempLo[0];
+  uint32_t tempRaw = (dataTempHi[0] << 8) | dataTempLo[0];
   int tempInt = (int) tempRaw;
   float temp = (float) tempInt;
   temp = temp / 256.0f;
@@ -287,9 +292,9 @@ void loop()
 #else
   // Humidity sensor
   uint32_t dataHum [2];
-  Hub.getSensor (2, dataHum);
+  Hub.getSensor(hum, dataHum);
 
-  int16_t hum = (int16_t) ((dataHum[1] << 8) | dataHum[0]);
+  int16_t hum = (int16_t)((dataHum[1] << 8) | dataHum[0]);
 
   float fhum = (float) hum;
   float rh = Linear_Interpolation(&humLine, fhum);
